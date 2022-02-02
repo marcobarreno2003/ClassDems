fc2edbe99370ae05360a5cfa2e6801acc5c35496
Marco Barreno <barrendkmarco@gmail.com>
2022-02-01 17:23:07 -0700

    Second Commit

---
+++ lab-04-ronald-marco-main/Lib/Class1.cs
@@ -0,0 +1,39 @@
+﻿namespace Lib;
+
+public interface Product
+{
+
+
+    int Ram();
+    int Selection(int numberSelector);
+    int Storage();
+}
+public abstract class Device : Product
+{
+    public abstract int Ram();
+
+
+    public abstract int Storage();
+    public abstract int Display();
+    public abstract int Selection(int numberSelector);
+    
+
+
+}
+    public abstract class Laptop : Device
+    {
+        public abstract bool Brand();
+        public override int Ram()
+        {
+            return 32;
+        }
+    }
+    public abstract class Phone : Device
+    {
+        public override int Ram()
+        {
+            return 6;
+        }
+    }
+
+
